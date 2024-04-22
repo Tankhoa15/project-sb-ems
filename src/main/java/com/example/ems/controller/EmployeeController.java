8cafc6befb04618a3f4671ad435e9f036df1fea8
Tankhoa15 <tankhoa1503kh@gmail.com>
2024-04-23 00:16:20 +0700

    create all package and deloy

---
+++ src/main/java/com/example/ems/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.example.ems.controller;
+
+import com.example.ems.dto.EmployeeDto;
+import com.example.ems.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+}
